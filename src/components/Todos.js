89179620b5a483c05255794071edafc2f3b6bb79
AlekseyTyshkul <aleksey.tyshkul@techsee.me>
2021-09-13 14:12:53 +0300

    added updateTodos

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { connect } from 'react-redux';
-import { addTodos, removeTodos } from '../redux/reducer';
+import { addTodos, removeTodos, updateTodos } from '../redux/reducer';
 
 const mapStateToProps = (state) => {
     return {
@@ -11,12 +11,27 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
     return {
         addTodo: (obj) => dispatch(addTodos(obj)),
-        removeTodos: (id) => dispatch(removeTodos(id))
+        removeTodos: (id) => dispatch(removeTodos(id)),
+        updateTodos: (obj) => dispatch(updateTodos(obj)),
     };
 };
 
 const Todos = (props) => {
     const [todo, setTodo] = useState("");
+
+    const inputRef = useRef(true);
+
+    const changeFocus = () => {
+        inputRef.current.disabled = false;
+        inputRef.current.focus();
+    }
+
+    const update = (id, value, e) => {
+        if (e.wich === 13) {
+            props.updateTodos({id, item: value});
+            inputRef.current.disabled = true;
+        }
+    };
 
     const handleChange = (e) => {
         setTodo(e.target.value);
@@ -42,7 +57,22 @@ const Todos = (props) => {
             <br />
             <ul>
                 {props.todos.map(item => {  
-                    return <li key={item.id}>{item.item} <button onClick={() => props.removeTodos(item.id)}>Delete</button></li>;
+                    return (
+                    <li key={item.id}>
+                        <textarea 
+                            ref={inputRef} 
+                            disabled={inputRef}
+                            defaultValue={item.item}
+                            onKeyPress={(e) => update(item.id, inputRef.current.value, e)}
+                        />
+                        <button onClick={() => changeFocus()}>
+                            Edit
+                        </button>
+                        <button onClick={() => props.removeTodos(item.id)}>
+                            Delete
+                        </button>
+                    </li>
+                    );
                 })}
             </ul>
         </div>
